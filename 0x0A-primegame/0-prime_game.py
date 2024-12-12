0f18d1130e585033628bd14609e4f9d0bb1b05a9
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-12-12 15:29:39 +0200

    Add solution of prime game project

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Prime Game Module'''
 
+
 def isWinner(x, nums):
     '''where x is the number of rounds and nums is an array of n
     Return: name of the player that won the most rounds
